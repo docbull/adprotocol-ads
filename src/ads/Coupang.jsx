2186f9c4dbdf1e6523c40f721092650a9eb0201d
docbull <fudghks21@gmail.com>
2025-05-22 23:16:45 +0900

    build

--- src/ads/Coupang.jsx
+++ src/ads/Coupang.jsx
@@ -2,33 +2,48 @@ import React from "react";
 import styled from "styled-components";
 import { useEffect, useState, useRef } from "react";
 
+
+// 반응형
+// 기본 3개, 600px 이하일 때 2개, 
+// 모바일(400px 이하)에서 디자인 2 적용
+
 const Coupang = ({ category }) => {
+    const [ isMobile, setIsMobile ] = useState(false);
     const [ items, setItems ] = useState([]);
 
     // 베스트 상품을 받아서 화면에 띄움
     useEffect(() => {
+        var start = Date.now();
         fetch(`https://cool-pony-c67e5b.netlify.app/.netlify/functions/coupang`, {
             method: "POST",
             body: JSON.stringify({ category: category }),
         })
         .then(res => res.json())
         .then(data => {
-            setItems(data.items);
+            const iframeWidth = Number(document.body.scrollWidth);
+            console.log(iframeWidth <= 400);
+
+            console.log("Netlify latency:", Date.now() - start);
+
+            if (iframeWidth <= 400) {
+                setIsMobile(true);
+                setItems(data.items);
+            } else if (iframeWidth <= 600) {
+                // show only 2 items...
+                setItems(data.items);
+            } else {
+                setItems(data.items);
+            }
         })
         .catch(console.error);
 
         const sendHeight = () => {
             const height = document.body.scrollHeight;
-            console.log(height);
+
+            // find parents til no more parent
             window.parent.parent.postMessage({ type: "ladder-ad-height", height }, "*");
             // window.parent.postMessage({ type: "ladder-ad-height", height }, "*");
         }
-
-        // window.addEventListener("message", (e) => {
-        //     if (e.data?.type === "heightRequest") {
-        //         sendHeight();
-        //     }
-        // });
 
         window.addEventListener("load", sendHeight);
         window.addEventListener("resize", sendHeight);
@@ -43,25 +58,39 @@ const Coupang = ({ category }) => {
         window.open(url, "_blank");
     }
 
+    const testClick = () => {
+        console.log(document.body.scrollWidth);
+    }
+
     return (
         // <div style={{width: "40%"}}>
         <AdWrapper>
             {items.map((item, idx) => (
-                <CoupangItemWrapper key={idx}>
-                    <CoupangImageWrapper> 
-                        <CoupangImage src={item.productImage} />
-                    </CoupangImageWrapper>
-                    <CoupangDescription>
-                        <CoupangItemName> {item.productName} </CoupangItemName>
-                        <CoupangItemName>  ₩{item.productPrice.toLocaleString()} </CoupangItemName>
-                        <CoupangShippingWrapper>
-                            {item.isFreeShipping ? <CoupangFreeShipping> 무료배송 </CoupangFreeShipping> : <></>}
-                            {item.isRocket ? <CoupangRocketShipping> 빠른배송 </CoupangRocketShipping> : <></>}
-                        </CoupangShippingWrapper>
-                        <CoupangSeeDetails onClick={() => coupangClickEvent(item.productUrl)}> 지금 보러가기 </CoupangSeeDetails>
-                    </CoupangDescription>
-                 </CoupangItemWrapper>
+                !isMobile ? 
+                    <CoupangItemWrapper key={idx}>
+                        <CoupangImageWrapper> 
+                            <CoupangImage src={item.productImage} />
+                            <CoupangShippingWrapper>
+                                {item.isFreeShipping ? <CoupangFreeShipping> 무료배송 </CoupangFreeShipping> : <></>}
+                                {item.isRocket ? <CoupangRocketShipping> 빠른배송 </CoupangRocketShipping> : <></>}
+                            </CoupangShippingWrapper>
+                        </CoupangImageWrapper>
+                        <CoupangDescription>
+                            <CoupangItemName> {item.productName} </CoupangItemName>
+                            <CoupangItemName>  ₩{item.productPrice.toLocaleString()} </CoupangItemName>
+                            {/* <CoupangShippingWrapper>
+                                {item.isFreeShipping ? <CoupangFreeShipping> 무료배송 </CoupangFreeShipping> : <></>}
+                                {item.isRocket ? <CoupangRocketShipping> 빠른배송 </CoupangRocketShipping> : <></>}
+                            </CoupangShippingWrapper> */}
+                            <CoupangSeeDetails onClick={() => coupangClickEvent(item.productUrl)}> 지금 보러가기 </CoupangSeeDetails>
+                        </CoupangDescription>
+                    </CoupangItemWrapper>
+                :
+                    <CoupangItemWrapperMobile key={idx}>
+                        asfasdf
+                    </CoupangItemWrapperMobile>
             ))}
+            <div onClick={testClick} style={{position: "absolute", bottom: "0"}}>BUTTON</div>
         </AdWrapper>
         // </div>
     );
@@ -81,7 +110,7 @@ const AdWrapper = styled.div`
 `;
 
 const CoupangItemWrapper = styled.div`
-    width: 250px;
+    width: 200px;
     // width: 15rem;
     // height: 365px;
 
@@ -97,8 +126,8 @@ const CoupangItemWrapper = styled.div`
 `;
 
 const CoupangImageWrapper = styled.div`
-    // width: 100%;
-    // height: 100%;
+    position: relative;
+
     dispaly: flex;
     justify-content: center;
     align-items: center;
@@ -120,7 +149,7 @@ const CoupangDescription = styled.div`
 `;
 
 const CoupangItemName = styled.span`
-    margin: 0.3rem 0;
+    margin: 0.2rem 0;
 
     display: -webkit-box;
     -webkit-box-orient: vertical;
@@ -130,10 +159,15 @@ const CoupangItemName = styled.span`
     overflow: hidden;
     text-overflow: ellipsis;
 
-    font-size: 1.15rem;
+    font-size: 1.1rem;
 `;
 
 const CoupangShippingWrapper = styled.div`
+    position: absolute;
+    top: 0;
+    // bottom: 0;
+    // right: 0;
+
     display: flex;
 
     margin: 0.3rem 0;
@@ -153,6 +187,8 @@ const CoupangRocketShipping = styled.div`
     background: #EAF3FE;
     padding: 0.2rem 0.6rem;
 
+    font-size: 0.9rem;
+
     display: flex;
     align-items: center;
     justify-content: center;
@@ -168,11 +204,15 @@ const CoupangSeeDetails = styled.div`
     margin: 0.3rem 0;
 
     border-radius: 10px;
-    padding: 0.7rem;
+    padding: 0.5rem;
     background: #3878F2;
     color: white;
 
     &:hover {
         cursor: pointer;
     }
+`;
+
+const CoupangItemWrapperMobile = styled.div`
+
 `;
