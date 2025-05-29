3e962bb1953a7c6648e0931abe65429326ce8eea
docbull <fudghks21@gmail.com>
2025-05-29 20:59:20 +0900

    build

--- src/netlify/functions/finder.js
+++ src/netlify/functions/finder.js
@@ -96,22 +96,24 @@ const getRecomendedItems = async () => {
 }
 
 async function storeContent(content) {
-    const app = initializeApp({
-        apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-        authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-        storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-        messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-        appId: process.env.REACT_APP_FIREBASE_APPID,
-        measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
-    });
-
-    const firestore = getFirestore(app);
-
-    addDoc(collection(firestore, "ladder"), {
-        content: content,
-    }).then((res) => {
-        return res;
+    return new Promise((resolve) => {
+        const app = initializeApp({
+            apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+            authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+            projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+            storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+            messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+            appId: process.env.REACT_APP_FIREBASE_APPID,
+            measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
+        });
+    
+        const firestore = getFirestore(app);
+    
+        addDoc(collection(firestore, "ladder"), {
+            content: content,
+        }).then((res) => {
+            resolve(res);
+        });
     });
 }
 
